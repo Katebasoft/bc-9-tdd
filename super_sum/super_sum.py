d00ec67d9da6c257f0b20e038d5d3eca5afd0b4c
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-08-18 19:29:49 +0300

    Adds a complete working super_sum function

--- super_sum/super_sum.py
+++ super_sum/super_sum.py
@@ -1,5 +1,23 @@
-def super_sum():
-	pass
+def super_sum(*argv):
+	sum_numbers = 0
+	for arg in argv:
+		if type(arg) == list:
+			for num in arg:
+				sum_numbers += num
+		elif type(arg) == str:
+			return 0
+		else:
+			sum_numbers += arg
+			
+
+	if sum_numbers > 1000:
+		return "sum too big"
+	elif sum_numbers == 0:
+		return "sum too small"
+	return sum_numbers
+
+
+#print super_sum(10, 5, 6, 9)
 
 
 
