d7804611cc030fa1fabc024c365c9d02fee7ad64
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-08-18 18:14:10 +0300

    First commit

---
+++ sum/my_sum.py
@@ -0,0 +1,8 @@
+def my_sum(x,y):
+	'''
+	Takes in two numbers and sum
+	'''
+	if type(x) == int and type(y) == int:
+		return x + y
+	else:
+		return "You didn't pass numbers"
---
+++ sum/sum_test.py
@@ -0,0 +1,29 @@
+import unittest
+
+from my_sum import my_sum
+
+class MySumTests(unittest.TestCase):
+
+	def setUp(self):
+			self.numbers = [10,5,7,8,90,60]
+
+	def test_sum_of_numbers(self):
+		'''
+		Test sum of digits/numbers
+		'''
+		result = my_sum(5,10)
+		self.assertEqual(result, 15)
+		self.assertEqual(my_sum(10,15),25)
+		self.assertEqual(my_sum(-9,2), -7)
+
+	def test_non_numbers(self):
+		'''
+		Assert throwing of exceptions when it's a non-number
+		'''
+		self.assertEqual(my_sum('a', 'a'), "You didn't pass numbers")
+
+if __name__ == '__main__':
+	unittest.main()
+
+
+
