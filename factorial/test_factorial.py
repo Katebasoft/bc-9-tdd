0b01c773d361fc925374d5091938e8461c3896a0
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-08-19 17:55:33 +0300

    Removes factorial function from a class and makes it stand alone, adds a testcase to test factorial of a number

--- factorial/test_factorial.py
+++ factorial/test_factorial.py
@@ -1,11 +1,14 @@
 import unittest
-from factorial import Factorial
+from factorial import factorial
 
 class TestFactorial(unittest.TestCase):
     
     def test_factorial(self):
         #Test for 0
         self.assertEqual(factorial(0), 1, "Should return 1")
+
+    def test_factorial(self):
+        self.assertEqual(factorial(3), 6, "Wrong Answer")
 
      
 
